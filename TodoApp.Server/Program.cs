2d67dbecf28006528e7d7f93d8aa57c2da55e613
josh_mckenna <josh.mckenna@thinkingbig.net>
2024-02-28 14:55:00 -0400

    APi Converted to Minimal Web API
    for the unit testing I had change it from a controller-based api to a Minimal web API because I couldn't find a solution to uses NUnit for unit testing. I die inside thinking about this change. Anyways I had to remove all the logic from the controller and put it into the program.cs and remove lines of code that was used to setup the controllers

--- TodoApp.Server/Program.cs
+++ TodoApp.Server/Program.cs
@@ -1,30 +1,115 @@
+using Humanizer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoApp.Server.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace TodoApp.Server;
 
-// Add services to the container.
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    //Todo: Remove this line below and remove anything weather related once I get to making the TodoItemP app
+    builder.Services.AddControllers();
+    var connectionString = builder.Configuration.GetConnectionString("WebApiDatabase");
+    builder.Services.AddSqlite<TodoContext>(connectionString);
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    //Todo: Remove this line below
+    builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("WebApiDatabase");
+    builder.Services.AddSwaggerGen();
 
-builder.Services.AddSqlite<TodoContext>(connectionString);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    var app = builder.Build();
 
-var app = builder.Build();
+    RouteGroupBuilder todoItems = app.MapGroup("/TodoItems");
+
+    todoItems.MapGet("/", GetAllTodos);
+    todoItems.MapGet("/complete", GetCompleteTodos);
+    todoItems.MapGet("/{id}", GetTodo);
+    // todoItems.MapPost("/", CreateTodo);
+    todoItems.MapPut("/{id}", UpdateTodo);
+    todoItems.MapDelete("/{id}", DeleteTodo);
+
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+  }
+  static async Task<IResult> GetAllTodos(TodoContext dbP)
+  {
+    return TypedResults.Ok(await dbP.TodoItems.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+  }
+
+  static async Task<IResult> GetCompleteTodos(TodoContext dbP)
+  {
+    return TypedResults.Ok(await dbP.TodoItems.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
+  }
+
+  static async Task<IResult> GetTodo(int id, TodoContext dbP)
+  {
+    return await dbP.TodoItems.FindAsync(id)
+      is TodoItem todoitem
+      ? TypedResults.Ok(new TodoItemDTO(todoitem))
+      : TypedResults.NotFound();
+  }
+
+  // static async Task<IResult> CreateTodo(TodoItem TodoItemP, TodoItemDTO todoItemDTOP, TodoContext dbP)
+  // {
+  //   var todoItem = new TodoItem
+  //   {
+  //     IsComplete = todoItemDTOP.IsComplete,
+  //     Title = todoItemDTOP.Title,
+  //     Description = todoItemDTOP.Description
+  //   };
+  //
+  //   dbP.TodoItems.Add(todoItem);
+  //   await dbP.SaveChangesAsync();
+  //
+  //   todoItemDTOP = new TodoItemDTO(todoItem);
+  //
+  //   return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTOP);
+  // }
+
+  static async Task<IResult> UpdateTodo(int idP, TodoItemDTO todoItemDTOP, TodoContext dbP)
+  {
+    var todoItem = await dbP.TodoItems.FindAsync(idP);
+
+    if (todoItem is null) return TypedResults.NotFound();
+
+    todoItem.Title = todoItemDTOP.Title;
+    todoItem.Description = todoItemDTOP.Description;
+    todoItem.DateTime = todoItemDTOP.DateTime;
+    todoItem.IsComplete = todoItemDTOP.IsComplete;
+
+    await dbP.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+  }
+
+  static async Task<IResult> DeleteTodo(int id, TodoContext dbP)
+  {
+    if (await dbP.TodoItems.FindAsync(id) is TodoItem TodoItemP)
+    {
+      dbP.TodoItems.Remove(TodoItemP);
+      await dbP.SaveChangesAsync();
+      return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
+  }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
