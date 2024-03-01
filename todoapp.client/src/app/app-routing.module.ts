a73965c1a929057e680e5bc5f39bafeac7e55a41
josh_mckenna <josh.mckenna@thinkingbig.net>
2024-03-01 09:09:26 -0400

    Added Path for /weather

--- todoapp.client/src/app/app-routing.module.ts
+++ todoapp.client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {WeatherExampleComponent} from "./weather-example/weather-example.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'weather',
+  component: WeatherExampleComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
