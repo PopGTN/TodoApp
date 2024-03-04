f56d124e8f6a9deb2361efa0a34a2937ad1125a6
Joshua Mckenna <josh.mckenna@thinkingbig.net>
2024-03-04 12:01:14 -0400

    Fixes the Format of Program.cs

--- TodoApp.Server/Program.cs
+++ TodoApp.Server/Program.cs
@@ -7,43 +7,35 @@ using TodoApp.Server.DTOs;
 using TodoApp.Server.Interfaces;
 using TodoApp.Server.Modules;
 
-namespace TodoApp.Server;
-
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+//Todo: Remove this line below and remove anything weather related once I get to making the TodoItemP app
+builder.Services.AddControllers();
+//Registers TodoItems Module
+builder.Services.RegisterModules();
+builder.Services.AddDbContext<TodoContext>(opt =>
+  opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//Todo: Remove this line below
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-  public static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
-    // Add services to the container.
-    //Todo: Remove this line below and remove anything weather related once I get to making the TodoItemP app
-    builder.Services.AddControllers();
-    //Registers TodoItems Module
-    builder.Services.RegisterModules();
-    builder.Services.AddDbContext<TodoContext>(opt =>
-      opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-    //Todo: Remove this line below
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen(c =>
-    {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoList Api", Version = "v1" });
-    });
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoList Api", Version = "v1" });
+});
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    //Maps TodoItems Module Endpoints
-    app.MapEndpoints();
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-      app.UseSwagger();
-      app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
-    //TODO: Remove
-    app.MapControllers();
-    app.Run();
-  }
+//Maps TodoItems Module Endpoints
+app.MapEndpoints();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+//TODO: Remove
+app.MapControllers();
+app.Run();
