7884d87573f8d150a8b308ff7ce312425bb72db2
Joshua Mckenna <josh.mckenna@thinkingbig.net>
2024-03-04 13:16:39 -0400

    Reverts Last Commit because it broke the testing class

--- TodoApp.Server/Program.cs
+++ TodoApp.Server/Program.cs
@@ -7,35 +7,43 @@ using TodoApp.Server.DTOs;
 using TodoApp.Server.Interfaces;
 using TodoApp.Server.Modules;
 
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-//Todo: Remove this line below and remove anything weather related once I get to making the TodoItemP app
-builder.Services.AddControllers();
-//Registers TodoItems Module
-builder.Services.RegisterModules();
-builder.Services.AddDbContext<TodoContext>(opt =>
-  opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-//Todo: Remove this line below
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-  c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoList Api", Version = "v1" });
-});
+namespace TodoApp.Server;
 
-var app = builder.Build();
-
-//Maps TodoItems Module Endpoints
-app.MapEndpoints();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    //Todo: Remove this line below and remove anything weather related once I get to making the TodoItemP app
+    builder.Services.AddControllers();
+    //Registers TodoItems Module
+    builder.Services.RegisterModules();
+    builder.Services.AddDbContext<TodoContext>(opt =>
+      opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    //Todo: Remove this line below
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+      c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoList Api", Version = "v1" });
+    });
+
+    var app = builder.Build();
+
+    //Maps TodoItems Module Endpoints
+    app.MapEndpoints();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    //TODO: Remove
+    app.MapControllers();
+    app.Run();
+  }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-//TODO: Remove
-app.MapControllers();
-app.Run();
