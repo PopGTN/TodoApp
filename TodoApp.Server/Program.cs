51aa064d227d2ab60779e9cc604f256f5f3bf9d5
Joshua Mckenna <josh.mckenna@thinkingbig.net>
2024-03-06 10:05:08 -0400

    Code Clean up Program.cs:
    Changes:
     - I removed swagger and anything that wasn't required for the api to run.
     - Remove the the controller package as well as the WeatherForecastController.cs was deleted.
     - Cleaned up the TodoDB.db database

--- TodoApp.Server/Program.cs
+++ TodoApp.Server/Program.cs
@@ -1,49 +1,19 @@
-using Humanizer;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using TodoApp.Server.DTOs;
 using TodoApp.Server.Interfaces;
-using TodoApp.Server.Modules;
 
-namespace TodoApp.Server;
+var builder = WebApplication.CreateBuilder(args);
+//Registers All the Modules
+builder.Services.RegisterModules();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
+builder.Services.AddEndpointsApiExplorer();
+var app = builder.Build();
+//Maps TodoItems Module Endpoints
+app.MapEndpoints();
+//Forces Https to keep people looking at the information being sent and recieved
+app.UseHttpsRedirection();
+app.Run();
 
-public class Program
-{
-  public static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
-    // Add services to the container.
-    //Todo: Remove this line below and remove anything weather related once I get to making the TodoItemP app
-    builder.Services.AddControllers();
-    //Registers TodoItems Module
-    builder.Services.RegisterModules();
-    builder.Services.AddDbContext<TodoContext>(opt =>
-      opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-    //Todo: Remove this line below
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen(c =>
-    {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoList Api", Version = "v1" });
-    });
+public partial class Program { }
 
-    var app = builder.Build();
-
-    //Maps TodoItems Module Endpoints
-    app.MapEndpoints();
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-      app.UseSwagger();
-      app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
-    //TODO: Remove
-    app.MapControllers();
-    app.Run();
-  }
-}
