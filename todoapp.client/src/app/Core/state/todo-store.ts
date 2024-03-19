6d74c35ccb0bf4cd5d40ea6a47c6f65c4eb786db
Joshua Mckenna <josh.mckenna@thinkingbig.net>
2024-03-19 12:56:09 -0300

    Added Presistant Storage

--- todoapp.client/src/app/Core/state/todo-store.ts
+++ todoapp.client/src/app/Core/state/todo-store.ts
@@ -3,17 +3,29 @@ import {getEntity, selectEntity, setEntities, withEntities} from "@ngneat/elf-en
 import {TodoItem} from "../Models/TodoItems";
 import {Injectable} from "@angular/core";
 import {syncState} from "elf-sync-state";
+import {localStorageStrategy, persistState} from "@ngneat/elf-persist-state";
 
 export const todoStore = createStore(
-  { name: 'todoItems' },
+  {name: 'todoItems'},
   withEntities<TodoItem>(),
 );
 
-// syncState(todoStore);
+/*
+Persist State added. Might Work on making a queue where the use is offline and make them once they comeback online it
+all the changes will be then saved to the database.
+*/
+export const persist = persistState(todoStore, {
+  key: 'todoItems',
+  storage: localStorageStrategy,
+});
+
+
+syncState(todoStore);
+
 @Injectable({
   providedIn: 'root'
 })
-export class TodoRepo{
+export class TodoRepo {
 
 
   setTodos(todoItem: TodoItem[]) {
@@ -22,7 +34,7 @@ export class TodoRepo{
 
 
   getTodoByID(id: any) {
-    return  todoStore.pipe(selectEntity(id, { pluck: (todoItem) => todoItem.id }));
+    return todoStore.pipe(selectEntity(id, {pluck: (todoItem) => todoItem.id}));
 
   }
 }
