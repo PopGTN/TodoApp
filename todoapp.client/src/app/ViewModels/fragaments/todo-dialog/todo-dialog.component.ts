eb7a6d428874bc76602716326c34ea690b0e2425
Joshua Mckenna <josh.mckenna@thinkingbig.net>
2024-03-19 15:38:50 -0300

    made pase in string into dialogs to be put in has innerhtml for more flexibility

--- todoapp.client/src/app/ViewModels/fragaments/todo-dialog/todo-dialog.component.ts
+++ todoapp.client/src/app/ViewModels/fragaments/todo-dialog/todo-dialog.component.ts
@@ -1,11 +1,21 @@
-﻿import {Component, Inject, Input} from '@angular/core';
+﻿import {Component, inject, Inject, Input} from '@angular/core';
 import {NgbTooltip} from "@ng-bootstrap/ng-bootstrap";
 import {FormsModule} from "@angular/forms";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
-import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from "@angular/material/dialog";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {TodoItem} from "../../../Core/Models/TodoItems";
+import {DialogComponent} from "../dialog/dialog.component";
+import {DialogType} from "../dialog/Models/DialogType";
 
 @Component({
   selector: 'app-todo-dialog',
@@ -26,7 +36,8 @@ import {TodoItem} from "../../../Core/Models/TodoItems";
   ],
 })
 export class TodoDialogComponent {
-
+  public dialog = inject(MatDialog);
+  public errorMessage: string;
   constructor(
     public dialogRef: MatDialogRef<TodoDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: {
@@ -38,13 +49,43 @@ export class TodoDialogComponent {
       isComplete?: boolean,
       dialogTitle?: string
     },
-  ) {}
+  ) {
+    this.errorMessage = "Please sure your following imputs are valid! <br>";
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
-  }
-  onSubmit(){
+
+    if (!this.isValid()) {
+      let ErrorDialogRef = this.dialog.open(DialogComponent, {
+      data: {
+        dialogType: DialogType.Ok,
+        title: "🫸🛑 HOLD UP!",
+        description: this.errorMessage
+
+      },
+        height: 'fit-content',
+        width: 'fit-content',
+      });
+      let dialogResult = ErrorDialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+      dialogResult.unsubscribe();
+      return;
+    }
+
     let todoItem = new TodoItem(this.data.id, this.data.title, this.data.description, this.data.dateTime, this.data.isComplete)
+
     this.dialogRef.close(todoItem);
+  }
+
+  isValid(){
+    let isValid = true;
+    if (!this.data.title){
+      isValid = false;
+      this.errorMessage += " - Todo must have a title <br>"
+    }
+    return isValid;
+    /*Left Room to make more requirments here if needed!*/
   }
 }
